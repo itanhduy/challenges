6526a3e7ecca368ad72e8cc28e18aeb9787ea930
Andy Ng <itc.anhduy@gmail.com>
2018-09-21 15:49:07 +0700

    Added general styling

--- src/theme/General/index.js
+++ src/theme/General/index.js
@@ -1,3 +1,9 @@
+/**
+ * Define general values
+ * Andy Ng
+ * 2018-09-21 15:48:56
+ */
+
 const General = {
   fullWidth: {
     width: '100%',
@@ -6,3 +12,5 @@ const General = {
     height: '100%',
   },
 }
+
+export default General
