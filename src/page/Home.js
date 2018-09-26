577048cba70b17c0b2742e1201fb5835fe41be92
Andy Ng <itc.anhduy@gmail.com>
2018-09-26 12:06:13 +0700

    Able to start responsive function in the first time

--- src/page/Home.js
+++ src/page/Home.js
@@ -60,7 +60,7 @@ class Home extends PureComponent {
           })
         },
       },
-    ])
+    ]).start()
   }
 
   /**
--- src/theme/Responsive/index.js
+++ src/theme/Responsive/index.js
@@ -1,5 +1,26 @@
 const Responsive = {
   /**
+   * @param {Array} arrayListen Array of conditions need to detect
+   * @param {Number} minWidth The min width
+   * @param {Number} maxWidth The max width
+   * @param {Function} inCase The callback will be called when currentWidth is high than minWidth and currentWidth low than maxWidth
+   * @param {Function} outCase The callback will be called when not fit with those conditions below
+   * Check current width with conditions
+   * Call inCase() when condition of minWidth anx maxWidth are correct
+   * Otherwise call outCase()
+   */
+  startResponsive: arrayListen => {
+    const currentWidth = window.outerWidth
+    arrayListen.forEach(condition => {
+      const { minWidth = 0, maxWidth = window.outerWidth, inCase, outCase } = condition
+      if (currentWidth > minWidth && currentWidth <= maxWidth) {
+        if (inCase) inCase()
+      } else {
+        if (outCase) outCase()
+      }
+    })
+  },
+  /**
    * Callback when view change size
    * @param {Array} arrayListen Array of conditions need to detect
    * @param {Number} minWidth The min width
@@ -8,16 +29,20 @@ const Responsive = {
    * @param {Function} outCase The callback will be called when not fit with those conditions below
    */
   listen: arrayListen => {
+    /**
+     * Listening to window resize change event
+     * Trigger the function `startResponsive` when size of window was changed
+     */
     window.onresize = () => {
-      const currentWidth = window.outerWidth
-      arrayListen.forEach(condition => {
-        const { minWidth = 0, maxWidth = window.outerWidth, inCase, outCase } = condition
-        if (currentWidth > minWidth && currentWidth <= maxWidth) {
-          if (inCase) inCase()
-        } else {
-          if (outCase) outCase()
-        }
-      })
+      Responsive.startResponsive(arrayListen)
+    }
+    return {
+      /**
+       * Trigger event `onresize` for the first time
+       */
+      start: () => {
+        Responsive.startResponsive(arrayListen)
+      },
     }
   },
 }
